671f20911ce01dfd84a94b0548e3a05be18b2086
Dennis Chou <dennischou@mba2.local>
2024-10-26 08:06:00 -0700

    new version

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 # setup.py
 from setuptools import setup, find_packages
 
+# Read in the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="functionmonitor",                     # The package name
-    version="0.1.3",                            # Initial version number
+    version="0.1.4",                            # Initial version number
     description="A tool for monitoring and managing asynchronous function execution.",
-    author="Dennis Chou",                         # Your name
-    author_email="dennischou@gmail.com",      # Your email
+    long_description=long_description,          # Use the README as the long description
+    long_description_content_type="text/markdown",  # Specify Markdown format for PyPI
+    author="Dennis Chou",                       # Your name
+    author_email="dennischou@gmail.com",        # Your email
     url="https://github.com/djchou/functionmonitor",  # URL to your repository
     packages=find_packages(),                   # Automatically find sub-packages
     install_requires=[                          # Dependencies
